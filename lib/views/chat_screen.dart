4ed18a20fcccd3699c97dcfaf5a54c1cd7d4c560
FernandoMoreti <fernandombolela@gmail.com>
2026-06-10 21:36:45 -0300

    ojeoja

--- lib/views/chat_screen.dart
+++ lib/views/chat_screen.dart
@@ -2,63 +2,7 @@
 
 import 'package:flutter/material.dart';
 import '../services/GeminiService.dart';
-
-// lib/model/places.dart
-
-class PontoTuristico {
-  final String nome;
-  final String endereco;
-  final String categoria;
-  final int tempoMinutos;
-  final String descricao;
-  final double latitude;
-  final double longitude;
-  final double rating;
-  final double distanciaMetros;
-  final String? photoReference;
-  final String placeId;
-
-  PontoTuristico({
-    required this.nome,
-    required this.endereco,
-    required this.categoria,
-    required this.tempoMinutos,
-    required this.descricao,
-    required this.latitude,
-    required this.longitude,
-    this.rating = 0.0,
-    this.distanciaMetros = 0.0,
-    this.photoReference,
-    this.placeId = '',
-  });
-
-  String get distanciaFormatada {
-    if (distanciaMetros < 1000) {
-      return '${distanciaMetros.toStringAsFixed(0)}m';
-    } else {
-      return '${(distanciaMetros / 1000).toStringAsFixed(1)}km';
-    }
-  }
-
-  factory PontoTuristico.mesclar(
-    Map<String, dynamic> dadosGoogle,
-    Map<String, dynamic> dadosGemini,
-  ) {
-    return PontoTuristico(
-      nome: dadosGoogle['nome'] ?? 'Lugar sem nome',
-      endereco: dadosGemini['endereco'] ?? dadosGoogle['endereco'] ?? 'Endereço não informado',
-      categoria: dadosGemini['categoria'] ?? 'Passeio',
-      tempoMinutos: dadosGemini['tempoMinutos'] ?? 60,
-      descricao: dadosGemini['descricao'] ?? 'Sem descrição disponível.',
-      latitude: (dadosGoogle['latitude'] ?? 0.0).toDouble(),
-      longitude: (dadosGoogle['longitude'] ?? 0.0).toDouble(),
-      rating: (dadosGoogle['rating'] ?? 0.0).toDouble(),
-      distanciaMetros: (dadosGoogle['distanciaMetros'] ?? 0.0).toDouble(),
-      photoReference: dadosGoogle['photoReference'],
-      placeId: dadosGoogle['placeId'] ?? '',
-    );
-  }
-}
+import 'package:tour_inteligente/model/places.dart';
 
 class ChatAssistenteScreen extends StatefulWidget {
   final PontoTuristico local;
--- lib/views/map_screen.dart
+++ lib/views/map_screen.dart
@@ -4,68 +4,12 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:tour_inteligente/model/places.dart';
 import '../services/LocaleService.dart';
 import '../services/PlaceService.dart';
 import '../services/GeminiService.dart';
 import '../services/AppConfig.dart';
 import 'chat_screen.dart';
-
-// lib/model/places.dart
-
-class PontoTuristico {
-  final String nome;
-  final String endereco;
-  final String categoria;
-  final int tempoMinutos;
-  final String descricao;
-  final double latitude;
-  final double longitude;
-  final double rating;
-  final double distanciaMetros;
-  final String? photoReference;
-  final String placeId;
-
-  PontoTuristico({
-    required this.nome,
-    required this.endereco,
-    required this.categoria,
-    required this.tempoMinutos,
-    required this.descricao,
-    required this.latitude,
-    required this.longitude,
-    this.rating = 0.0,
-    this.distanciaMetros = 0.0,
-    this.photoReference,
-    this.placeId = '',
-  });
-
-  String get distanciaFormatada {
-    if (distanciaMetros < 1000) {
-      return '${distanciaMetros.toStringAsFixed(0)}m';
-    } else {
-      return '${(distanciaMetros / 1000).toStringAsFixed(1)}km';
-    }
-  }
-
-  factory PontoTuristico.mesclar(
-    Map<String, dynamic> dadosGoogle,
-    Map<String, dynamic> dadosGemini,
-  ) {
-    return PontoTuristico(
-      nome: dadosGoogle['nome'] ?? 'Lugar sem nome',
-      endereco: dadosGemini['endereco'] ?? dadosGoogle['endereco'] ?? 'Endereço não informado',
-      categoria: dadosGemini['categoria'] ?? 'Passeio',
-      tempoMinutos: dadosGemini['tempoMinutos'] ?? 60,
-      descricao: dadosGemini['descricao'] ?? 'Sem descrição disponível.',
-      latitude: (dadosGoogle['latitude'] ?? 0.0).toDouble(),
-      longitude: (dadosGoogle['longitude'] ?? 0.0).toDouble(),
-      rating: (dadosGoogle['rating'] ?? 0.0).toDouble(),
-      distanciaMetros: (dadosGoogle['distanciaMetros'] ?? 0.0).toDouble(),
-      photoReference: dadosGoogle['photoReference'],
-      placeId: dadosGoogle['placeId'] ?? '',
-    );
-  }
-}
 
 class MapaScreen extends StatefulWidget {
   const MapaScreen({super.key});
