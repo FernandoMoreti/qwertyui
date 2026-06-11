41183078f7f07ec427680c581ff468a1ee1fa65a
FernandoMoreti <fernandombolela@gmail.com>
2026-06-10 21:11:06 -0300

    ojeoja

--- lib/views/map_screen.dart
+++ lib/views/map_screen.dart
@@ -4,7 +4,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
-import '../model/places.dart';
+import '../Model/places.dart';
 import '../services/LocaleService.dart';
 import '../services/PlaceService.dart';
 import '../services/GeminiService.dart';
